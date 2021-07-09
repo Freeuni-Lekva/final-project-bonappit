a257ca22d9fadfee74a1cca4c6df6e43e56b387c
saba <sburn18@freeuni.edu.ge>
2021-07-09 15:34:04 +0400

    restaurantDao

--- src/main/java/database/RestaurantsDao.java
+++ src/main/java/database/RestaurantsDao.java
@@ -7,16 +7,14 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import java.sql.*;
 
 public class RestaurantsDao {
-private Connection connection;
-private static final String insertSt = "insert into restaurants values (?, ?, ?,?);";
-    public RestaurantsDao(Connection connection){
-        this.connection = connection;
-        try {
-            PreparedStatement st = connection.prepareStatement("USE DB_restaurants");
-            st.execute();
-        } catch (SQLException throwable) {
-            throwable.printStackTrace();
-        }
+    Statement statement;
+    private Connection connection;
+    private static final String insertSt = "insert into restaurants values (?, ?, ?,?);";
+    private static final String insertUser = "insert into users values (?, ?, ?,?);";
+
+
+    public RestaurantsDao(){
+
     }
 
     //connector code
@@ -25,10 +23,10 @@ private static final String insertSt = "insert into restaurants values (?, ?, ?,
         DB_restaurants db_products = new DB_restaurants();
         BasicDataSource dataSource = db_products.getDataSoruce();
 
-        Connection connection = null;
+        connection = null;
         try {
             connection = dataSource.getConnection();
-            Statement statement = connection.createStatement();
+            statement = connection.createStatement();
             String tempString = "select * from " + str + ";";
             result = statement.executeQuery(tempString);
         } catch (SQLException throwables) {
@@ -37,25 +35,62 @@ private static final String insertSt = "insert into restaurants values (?, ?, ?,
         return result;
     }
 
-    public void addRestaurant(Restaurant res) {
+
+    //gets product from sql database by id
+    public User getUserByUsername(String thisUserName){
+        String strForResultSet = "";
+        User user = null;
+        if (user.isAdmin()){
+            strForResultSet = "restaurants";
+        } else {
+            strForResultSet = "users";
+        }
+        ResultSet result = getResultSet(strForResultSet);
+
         try {
-            PreparedStatement ps = connection.prepareStatement(insertSt);
-            ps.setString(1, res.getId());
-            ps.setString(2, res.getName());
-            ps.setString(3, res.getMenu());
+            while (result.next()) {
+                String currUserName = result.getString("username");
+                if (currUserName.equals(thisUserName)) {
+                    String username = result.getString("username");
+                    String password = result.getString("password");
+                    boolean isAdmin = result.getBoolean("type");
+                    int restaurantId = result.getInt("restaurantid");
+
+                    user = new User(username, password, isAdmin, restaurantId);
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return user;
+    }
+
+    public void updateStatement(User user) throws SQLException {
+        try {
+            PreparedStatement ps = connection.prepareStatement(insertUser);
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getPassword());
+            ps.setBoolean(3, user.isAdmin());
+            ps.setInt(4, user.getRestaurantId());
 
             ps.executeUpdate();
         } catch (SQLException throwable) {
             throwable.printStackTrace();
         }
     }
-//    public void updateStatement(User user) throws SQLException {
-//
-//        String command = "INSERT INTO metropolises VALUES (" + "\"" + metropolis.getMetropolis() + "\"" + "," +
-//                "\"" + metropolis.getContinent() + "\"" + "," +
-//                metropolis.getPopulation() + ");";
-//
-//        statement.executeUpdate(command);
-//    }
+
+    public void addRestaurant(Restaurant res) {
+        try {
+            PreparedStatement ps = connection.prepareStatement(insertSt);
+            ps.setString(1, res.getId());
+            ps.setString(2, res.getName());
+            ps.setString(3, res.getMenu());
+            ps.setInt(4, res.getNumTable());
+
+            ps.executeUpdate();
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+    }
 
 }
